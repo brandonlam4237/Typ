134cc42208bef89c9262ea7dd9e56137383bc768
Brandon Lam <brandonlam4237@gmail.com>
2023-05-03 14:34:54 -0400

    feat: add basic key event handler for matching char to text prompt

--- src/client/pages/Home.tsx
+++ src/client/pages/Home.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import { useEffect, useState } from "react";
 import SettingsBar from "../components/SettingsBar";
 import "../scss/home.scss";
 import reset from "../assets/reset.svg";
@@ -8,19 +8,49 @@ function Home() {
     "lorem ipsum dolor sit amet consectetur adipiscing elit sed do eiusmod tempor incididunt ut labore et dolore magna aliqua ut enim ad minim veniam quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur";
   const textArr: string[] = text.split(" ");
 
+  const [gameInProgress, setGameInProgress] = useState(false);
+  const [textIndex, setTextIndex] = useState(0);
+
+  useEffect(() => {
+    const handleKey = (e: any) => {
+      console.log(e.key);
+      if (e.key.length === 1 && e.key !== " ") {
+        if (!gameInProgress) setGameInProgress(true);
+      }
+      if (e.key === text[textIndex]) {
+        console.log(e.key, "matches", text[textIndex]);
+        setTextIndex(textIndex + 1);
+      }
+    };
+
+    document.addEventListener("keydown", handleKey);
+
+    // clean up event listener
+    return () => {
+      document.removeEventListener("keydown", handleKey);
+    };
+  }, [textIndex]);
+
   return (
     <main className="home">
       <SettingsBar />
       <div className="game">
-        <div className="game__timer">53</div>
+        {gameInProgress && <div className="game__timer">53</div>}
+        {!gameInProgress && (
+          <div className="game__timer game__timer--inactive">53</div>
+        )}
         <div className="game__container">
           <div className="game__cursor"></div>
           <div className="game__words">
-            {textArr.map((word) => {
+            {textArr.map((word, i) => {
               return (
-                <div className="game__word">
-                  {word.split("").map((letter) => {
-                    return <span className="game__letter">{letter}</span>;
+                <div className="game__word" key={i}>
+                  {word.split("").map((letter, j) => {
+                    return (
+                      <span className="game__letter" key={j}>
+                        {letter}
+                      </span>
+                    );
                   })}{" "}
                 </div>
               );

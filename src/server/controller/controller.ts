19dcff0ff7d61820a690067386ca4cf7dc788710
alan.tepoxtecatl@gmail.com <alan.tepoxtecatl@gmail.com>
2023-05-07 10:39:44 -0400

    Basic queries to for selecting Tables

---
+++ src/server/controller/controller.ts
@@ -0,0 +1,18 @@
+import { Request, Response } from "express";
+import pool from "../database/db";
+
+const getUsers = (req: Request, res: Response) => {
+  pool.query("SELECT * FROM users", (error, results) => {
+    if (error) throw error;
+    res.status(200).json(results.rows);
+  });
+};
+
+const getPhrases = (req: Request, res: Response) => {
+  pool.query("SELECT * FROM phrases", (error, results) => {
+    if (error) throw error;
+    res.status(200).json(results.rows);
+  });
+};
+
+export default { getUsers, getPhrases };

93de29e9674a471df9242aefbf21e6a1cb64fc56
alan.tepoxtecatl@gmail.com <alan.tepoxtecatl@gmail.com>
2023-05-08 16:34:04 -0400

    implemented the correct model for phrases

--- src/server/models/phrase.js
+++ src/server/models/phrase.js
@@ -1,21 +1,25 @@
-require("dotenv").config();
 const { Sequelize, DataTypes } = require("sequelize");
 const sequelize = new Sequelize(
-    "postgres://vseqcjmo:jpBip-0subWpZcQbDZyN-g_PMpl_YZ6i@lallah.db.elephantsql.com/vseqcjmo",
-    {
-        dialect: "postgres",
-    }
+  "postgres://vseqcjmo:jpBip-0subWpZcQbDZyN-g_PMpl_YZ6i@lallah.db.elephantsql.com/vseqcjmo",
+  {
+    dialect: "postgres",
+  }
 );
 
-const PhraseSchema = sequelize.define("Phrase", {
-    // Model Attributes defined here
-    text: {
-        type: DataTypes.STRING,
-        allowNull: false,
-    },
-    length: {
-        type: DataTypes.INTEGER,
-    },
+const Phrase = sequelize.define("Phrase", {
+  phrase_id: {
+    type: DataTypes.INTEGER,
+    primaryKey: true,
+    autoIncrement: true,
+  },
+  phrase: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+}, {
+  tableName: "phrases", 
+  timestamps: false, 
 });
 
-module.exports = sequelize.model("Phrase", PhraseSchema);
+module.exports = Phrase;
+

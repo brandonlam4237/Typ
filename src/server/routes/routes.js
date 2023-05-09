32902673de789684e36cdb0d5ee77409ce0cc312
tryingtolearn11 <damiensingh17@gmail.com>
2023-05-09 12:05:19 -0400

    Added PopulatePhrase handler & Changed RandomPhrases

--- src/server/routes/routes.js
+++ src/server/routes/routes.js
@@ -5,12 +5,12 @@ const router = express.Router();
 const phrase_controller = require("../controllers/phraseController");
 const users_controller = require("../controllers/usersController");
 
-
-
 //GET Endpoints for phrases
 router.get("/phrases", phrase_controller.getAllPhrases);
+router.get("/phrases/populate", phrase_controller.populatePhrases);
+router.get("/phrases/random", phrase_controller.getRandomPhrase);
+router.get("/phrases/delete/all", phrase_controller.deleteAllPhrases);
 router.get("/phrases/:id", phrase_controller.getPhraseByID);
-router.get("/random-phrase", phrase_controller.getRandomPhrase);
 
 //GET Endpoints for users.
 router.get("/users", users_controller.getAllUsers);

f3e3cb48987b0f49db8f41269bb01000ce836898
alan.tepoxtecatl@gmail.com <alan.tepoxtecatl@gmail.com>
2023-05-09 00:05:30 -0400

    added get random phrase function and endpoint

--- src/server/controllers/phraseController.js
+++ src/server/controllers/phraseController.js
@@ -10,6 +10,17 @@ exports.getAllPhrases= asyncHandler(async (req, res, next) => {
 exports.getPhraseByID = asyncHandler(async (req,res, next)=>{
     const id = parseInt(req.params.id);
     const phrase = await Phrase.findByPk(id);
+    if (phrase == null){
+        res.send("Phrase not found or does not exist");
+    }
     return res.status(200).json(phrase);
+});
+
+exports.getRandomPhrase = asyncHandler(async (req, res, next)=>{
+    const phrases = await Phrase.findAll();
+    const rand_num = Math.floor(Math.random()* phrases.length)+1;
+    console.log(rand_num);
+    const rand_phrase = await Phrase.findByPk(rand_num);
+    return res.status(200).json(rand_phrase);
 });
 
--- src/server/routes/routes.js
+++ src/server/routes/routes.js
@@ -5,21 +5,14 @@ const router = express.Router();
 const phrase_controller = require("../controllers/phraseController");
 const users_controller = require("../controllers/usersController");
 
-// Home Page
-router.get("/", function (req, res) {
-    res.send("HOME PAGE");
-});
 
-// USER ROUTES
-//router.get("/users", controller.getUsers);
 
-// PHRASE ROUTES
-//router.get("/phrases", controller.getPhrases);
-
-//GET for all phrases.
+//GET Endpoints for phrases
 router.get("/phrases", phrase_controller.getAllPhrases);
 router.get("/phrases/:id", phrase_controller.getPhraseByID);
+router.get("/random-phrase", phrase_controller.getRandomPhrase);
 
+//GET Endpoints for users.
 router.get("/users", users_controller.getAllUsers);
 router.get("/users/:id", users_controller.getUserByID);
 

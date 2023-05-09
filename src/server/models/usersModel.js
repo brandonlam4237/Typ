f2dd6622d341445d3b81497e09b03d6187f71335
alan.tepoxtecatl@gmail.com <alan.tepoxtecatl@gmail.com>
2023-05-08 22:36:57 -0400

    Implemented model for users table

---
+++ src/server/models/usersModel.js
@@ -0,0 +1,38 @@
+const { Sequelize, DataTypes } = require("sequelize");
+const { flattenDiagnosticMessageText } = require("typescript");
+const sequelize = new Sequelize(
+  "postgres://vseqcjmo:jpBip-0subWpZcQbDZyN-g_PMpl_YZ6i@lallah.db.elephantsql.com/vseqcjmo",
+  {
+    dialect: "postgres",
+  }
+);
+
+//Defines the model of our table which we already made in ElephantSQL
+const Users = sequelize.define("Users", {
+  user_id: {
+    type: DataTypes.INTEGER,
+    primaryKey: true,
+    autoIncrement: true,
+  },
+  username: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  password:{
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  email:{
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  creationdate:{
+    type: DataTypes.DATE
+  }
+
+}, {
+  tableName: "users", 
+  timestamps: false, 
+});
+
+module.exports = Users;

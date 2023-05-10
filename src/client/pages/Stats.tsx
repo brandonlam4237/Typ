1319d37f88e5b0b73646fab79798548c4af85c74
Brandon Lam <brandonlam4237@gmail.com>
2023-05-10 16:23:09 -0400

    feat: user stats grid layout

--- src/client/pages/Stats.tsx
+++ src/client/pages/Stats.tsx
@@ -1,54 +1,59 @@
 import userIcon from "../assets/account.svg";
+import "../scss/stats.scss";
 
 function Stats() {
   return (
     <main className="stats">
       <div className="stats__grid">
-        <div className="stats__grid-block">
-          <img src={userIcon} />
-          <p>Username</p>
-          <p>Joinged 26 Apr 2023</p>
+        <div className="stats__grid-block-1">
+          <div className="stats__user">
+            <img src={userIcon} className="stats__user-icon" />
+            <p className="stats__title">Username</p>
+          </div>
+          <p style={{ color: "#757575" }}>Joined 26 Apr 2023</p>
+
           <div>
-            <p>Tests Completed</p>
-            <p>13</p>
+            <p className="stats__title">Tests Completed</p>
+            <p className="stats__highlight stats__num-big">13</p>
           </div>
           <div>
-            <p>Time Typing</p>
-            <p>00:26:15</p>
+            <p className="stats__title">Time Typing</p>
+            <p className="stats__highlight stats__num-big">00:26:15</p>
+          </div>
+          <button className="stats__logout-btn">Logout</button>
+        </div>
+        <div className="stats__grid-block">
+          <div className="stats__grid-block-col">
+            <p className="stats__title">15 seconds</p>
+            <p className="stats__highlight stats__num-big">93</p>
+            <p className="stats__highlight">98%</p>
+          </div>
+          <div className="stats__grid-block-col">
+            <p className="stats__title">30 seconds</p>
+            <p className="stats__highlight stats__num-big">87</p>
+            <p className="stats__highlight">96%</p>
+          </div>
+          <div className="stats__grid-block-col">
+            <p className="stats__title">60 seconds</p>
+            <p className="stats__highlight stats__num-big">-</p>
+            <p className="stats__highlight">-</p>
           </div>
         </div>
         <div className="stats__grid-block">
-          <div>
-            <p>15 seconds</p>
-            <p>93</p>
-            <p>98%</p>
+          <div className="stats__grid-block-col">
+            <p className="stats__title">10 words</p>
+            <p className="stats__highlight stats__num-big">103</p>
+            <p className="stats__highlight">99%</p>
           </div>
-          <div>
-            <p>30 seconds</p>
-            <p>87</p>
-            <p>96%</p>
+          <div className="stats__grid-block-col">
+            <p className="stats__title">25 words</p>
+            <p className="stats__highlight stats__num-big">-</p>
+            <p className="stats__highlight">-</p>
           </div>
-          <div>
-            <p>60 seconds</p>
-            <p>-</p>
-            <p>-</p>
-          </div>
-        </div>
-        <div className="stats__grid-block">
-          <div>
-            <p>10 words</p>
-            <p>103</p>
-            <p>99%</p>
-          </div>
-          <div>
-            <p>25 words</p>
-            <p>-</p>
-            <p>-</p>
-          </div>
-          <div>
-            <p>50 words</p>
-            <p>91</p>
-            <p>97%</p>
+          <div className="stats__grid-block-col">
+            <p className="stats__title">50 words</p>
+            <p className="stats__highlight stats__num-big">91</p>
+            <p className="stats__highlight">97%</p>
           </div>
         </div>
       </div>

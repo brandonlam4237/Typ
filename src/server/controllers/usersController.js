7197b6f28c469112a1fe935101ae2c935a1f08fd
alan.tepoxtecatl@gmail.com <alan.tepoxtecatl@gmail.com>
2023-05-09 22:57:11 -0400

    Added authUser which authenticates user, addtionally added encryption for Passwords

--- src/server/controllers/usersController.js
+++ src/server/controllers/usersController.js
@@ -1,7 +1,7 @@
-const Phrase = require("../models/usersModel");
 const asyncHandler = require("express-async-handler");
 const Users = require("../models/usersModel");
-const { json } = require("sequelize");
+const bcrypt = require('bcrypt');
+
 
 //this get's all phrases from our model and returns as json.
 exports.getAllUsers = asyncHandler(async (req, res, next) => {
@@ -20,20 +20,51 @@ exports.getUserByID = asyncHandler(async (req,res, next)=>{
 });
 
 exports.addUser = asyncHandler(async (req,res, next)=>{
-    const {username, password, email, creationdate} = req.body;
-    //First check if the username or email is taken.
-    const found_username = await Users.findOne({where: {username:username}});
-    const found_email = await Users.findOne({where: {email:email}});
 
-    if (found_username != null || found_email != null){res.send("Username or password is already in use")}
+        const {username, password, email, creationdate} = req.body;
+
+        //First check if the username or email is taken.
+        const found_username = await Users.findOne({where: {username:username}});
+        const found_email = await Users.findOne({where: {email:email}});
+        if (found_username != null || found_email != null){res.send("Username or password is already in use")}
+        //Username and email are available
+        else{
+            try{
+                //Encrypt password
+                const encryptedPW = await bcrypt.hash(password, 10);
+
+                const date = new Date(); //Today's date
+                //Add user to DB.
+                const newUser = await Users.create({
+                    username: username, 
+                    password: encryptedPW, 
+                    email: email, 
+                    creationdate: date
+                })
+                console.log(`User created with user id: ${newUser.user_id}`); 
+
+            }
+            catch(error){
+                res.status(500).send(error)
+            }
+        }
+})
+
+
+exports.authUser = asyncHandler(async (req,res,next) =>{
+
+    const user = await Users.findOne({where: {email: req.body.email}})
+    if(user == null){return res.status(400).send('Cannot find user')}
     else{
-        const date = new Date();
-        const newUser = await Users.create({
-            username: username, 
-            password: password, 
-            email: email, 
-            creationdate: date
-        })
-        console.log(`User created with user id: ${newUser.user_id}`);   
+        try{
+            if(await bcrypt.compare(req.body.password, user.password)){
+                res.send('Successfully Logged In');
+            }
+            else{ res.send('Incorrect Password')};
+        
+        }catch{
+            res.status(500).send('Login Failed');
+        }
     }
+
 })

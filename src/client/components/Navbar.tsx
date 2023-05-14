37030c5c140630d364a07bce4d3f1147cfd941c6
Brandon Lam <brandonlam4237@gmail.com>
2023-05-14 14:52:23 -0400

    feat: user log in conditional path routing

--- src/client/components/Navbar.tsx
+++ src/client/components/Navbar.tsx
@@ -1,17 +1,23 @@
 import { Link } from "react-router-dom";
 import logo from "../assets/logo.png";
-import user from "../assets/account.svg";
+import user_icon from "../assets/account.svg";
 import "../scss/navbar.scss";
+import { useUserContext } from "../hooks/useUserContext";
 
 function Navbar() {
+  const { user } = useUserContext();
+
   return (
     <main className="navbar">
       <Link to="/">
         <img src={logo} className="navbar__logo" />
       </Link>
-      <Link to="/register">
-        <img src={user} className="navbar__user" />
-      </Link>
+      <div className="navbar__account">
+        {user && <p>{user.user.username}</p>}
+        <Link to="/register">
+          <img src={user_icon} className="navbar__user" />
+        </Link>
+      </div>
     </main>
   );
 }

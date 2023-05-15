2049bcefcfadaaad976687153097dc525ca3415d
Brandon Lam <brandonlam4237@gmail.com>
2023-05-15 14:56:36 -0400

    feat: add register form error handling

--- src/client/pages/Register.tsx
+++ src/client/pages/Register.tsx
@@ -8,16 +8,26 @@ function Register() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [passwordConfirm, setPasswordConfirm] = useState("");
+  const [error, setError] = useState("");
 
   const { login } = useLogin();
 
   async function handleRegister(e: any) {
     e.preventDefault();
+    if (password !== passwordConfirm) {
+      setError("passwords must match");
+      return;
+    }
     const response = await fetch("http://localhost:3000/api/users", {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify({ email, username, password }),
     });
+    const json = await response.json();
+    if (!response.ok) {
+      console.log(json.message);
+      setError(json.message);
+    }
     if (response.ok) {
       await login(email, password);
     }
@@ -80,6 +90,7 @@ function Register() {
         <Link to="/login">
           <p className="form__footer">Already have an account?</p>
         </Link>
+        {error && <div className="form__error">{error}</div>}
       </form>
     </main>
   );

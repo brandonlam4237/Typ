4efe5e60ac989663f57cd60a00c835afcfa5520f
Brandon Lam <brandonlam4237@gmail.com>
2023-05-16 20:29:21 -0400

    feat: add routing to leaderboard page with navbar link

--- src/client/components/Navbar.tsx
+++ src/client/components/Navbar.tsx
@@ -3,6 +3,7 @@ import logo from "../assets/logo.png";
 import user_icon from "../assets/account.svg";
 import "../scss/navbar.scss";
 import { useUserContext } from "../hooks/useUserContext";
+import leaderboard_icon from "../assets/leaderboard.png";
 
 function Navbar() {
   const { user } = useUserContext();
@@ -12,11 +13,17 @@ function Navbar() {
       <Link to="/">
         <img src={logo} className="navbar__logo" />
       </Link>
-      <div className="navbar__account">
-        {user && <p>{user.user.username}</p>}
-        <Link to="/register">
-          <img src={user_icon} className="navbar__user" />
+
+      <div className="navbar__options">
+        <Link to="/leaderboard">
+          <img src={leaderboard_icon} className="navbar__icon" />
         </Link>
+        <div className="navbar__account">
+          <Link to="/register">
+            <img src={user_icon} className="navbar__user" />
+          </Link>
+          {user && <p>{user.user.username}</p>}
+        </div>
       </div>
     </main>
   );
